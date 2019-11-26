550f507bd9f6639eec039546063617161dea7e24
mdloselo <mdloselo@gmail.com>
2019-11-26 13:29:41 +0200

    Updated JS and added new functions

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,15 @@
-module.exports = {
-  dependency: {
-    platforms: {
-      android: {
-        packageInstance: 'new RNBuildConfigPackage(BuildConfig.class)'
-      }
-    }
+import { NativeModules } from 'react-native';
+
+var RNBuildConfig = NativeModules.RNBuildConfig;
+
+export default {
+  getFlavor: function() {
+    return RNBuildConfig.getFlavor();
+  },
+  getVersionName: function() {
+    return RNBuildConfig.getVersionName();
+  },
+  getVersionCode: function() {
+    return RNBuildConfig.getVersionCode();
   }
 }
