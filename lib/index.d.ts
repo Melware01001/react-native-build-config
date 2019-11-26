24975f4da18354ece9483589db20b55b723ced2d
mdloselo <mdloselo@gmail.com>
2019-11-26 13:32:32 +0200

    Expo default functions that can be invoked on JS side

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,7 @@
-declare module 'react-native-build-config' {
-  const config: { [k: string]: any }
-  export default config
-}
+declare const _default: {
+  getFlavor: () => string;
+  getVersionName: () => string;
+  getVersionCode: () => string;
+};
+
+export default _default;
