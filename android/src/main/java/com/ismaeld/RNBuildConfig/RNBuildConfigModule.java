8163bfa3480b057c4204b9fc2fb8fc77a2faaedd
mdloselo <mdloselo@gmail.com>
2019-11-26 12:24:10 +0200

    Added additional functions to retrieve individual config values

--- android/src/main/java/com/ismaeld/RNBuildConfig/RNBuildConfigModule.java
+++ android/src/main/java/com/ismaeld/RNBuildConfig/RNBuildConfigModule.java
@@ -12,35 +12,86 @@ import java.util.Map;
 public class RNBuildConfigModule extends ReactContextBaseJavaModule {
     private Class buildConfigClass;
     private String NAME = "RNBuildConfig";
+    private final Map<String, Object> constants = new HashMap<>();
 
     public RNBuildConfigModule(ReactApplicationContext reactContext, Class buildConfigClass) {
         super(reactContext);
+
         this.buildConfigClass = buildConfigClass;
+
+        if(constants.isEmpty()) {
+            getConstants();
+        }
     }
 
     @Override
     public String getName() {
         return NAME;
     }
-    
+
+    @ReactMethod
+    public String getFlavor() {
+
+        try {
+
+            return this.constants.get("FLAVOR").toString();
+
+        } catch (Exception e) {
+
+            return null;
+
+        }
+
+    }
+
+    @ReactMethod
+    public String getVersionName() {
+
+        try {
+
+            return this.constants.get("VERSION_NAME").toString();
+
+        } catch (Exception e) {
+
+            return null;
+
+        }
+
+    }
+
+    @ReactMethod
+    public String getVersionCode() {
+
+        try {
+
+            return this.constants.get("VERSION_CODE").toString();
+
+        } catch (Exception e) {
+
+            return null;
+
+        }
+
+    }
+
     @ReactMethod
     public Map<String, Object> getConfig() {
-        return getConstants();
+        return this.constants;
     }
 
     @Override
     public Map<String, Object> getConstants() {
-        final Map<String, Object> constants = new HashMap<>();
 
         Field[] fields = buildConfigClass.getDeclaredFields();
         for (Field f : fields) {
             try {
-                constants.put(f.getName(), f.get(null));
+                this.constants.put(f.getName(), f.get(null));
             } catch (IllegalAccessException e) {
                 Log.d(NAME, "Could not access BuildConfig field " + f.getName());
             }
         }
 
-        return constants;
+        return this.constants;
+
     }
 }
